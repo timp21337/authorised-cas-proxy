318e887799e2a2b97427ac02e3bf3b38ccc7e33c
Tim Pizey <timp@paneris.org>
2011-03-18 17:39:43 +0000

    Eclipse warnings

--- src/test/java/net/pizey/cas/proxy/test/AuthorisedCasProxyTest.java
+++ src/test/java/net/pizey/cas/proxy/test/AuthorisedCasProxyTest.java
@@ -4,6 +4,7 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import net.pizey.cas.proxy.AuthorisedCasProxy;
+
 import org.apache.commons.cli.MissingOptionException;
 import org.apache.commons.cli.UnrecognizedOptionException;
 
@@ -11,50 +12,44 @@ import org.apache.commons.cli.UnrecognizedOptionException;
  * Unit test for simple AuthorisedCasProxy.
  */
 public class AuthorisedCasProxyTest
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AuthorisedCasProxyTest( String testName )
-    {
-        super( testName );
+    extends TestCase {
+  /**
+   * Create the test case
+   *
+   * @param testName name of the test case
+   */
+  public AuthorisedCasProxyTest(String testName) {
+    super(testName);
+  }
+
+  /**
+   * @return the suite of tests being tested
+   */
+  public static Test suite() {
+    return new TestSuite(AuthorisedCasProxyTest.class);
+  }
+
+  public void testCLI() throws Exception {
+    try {
+      AuthorisedCasProxy.configure(new String[] {});
+      fail("Should have bombed");
+    } catch (MissingOptionException e) {
+      e = null;
     }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AuthorisedCasProxyTest.class );
+    try {
+      AuthorisedCasProxy.configure(new String[] { "-u", "adam@example.org" });
+      fail("Should have bombed");
+    } catch (UnrecognizedOptionException e) {
+      e = null;
     }
-
-
-    public void testCLI() throws Exception {
-      AuthorisedCasProxy it = new AuthorisedCasProxy();
-      try {
-        it.configure(new String [] {});
-        fail("Should have bombed");
-      } catch (MissingOptionException e) {
-        e = null;
-      }
-      try {
-        it.configure(new String [] {"-u", "adam@example.org"});
-        fail("Should have bombed");
-      } catch (UnrecognizedOptionException e) {
-        e = null;
-      }
-      try {
-        it.configure(new String [] {"-us", "adam@example.org"});
-        fail("Should have bombed");
-      } catch (UnrecognizedOptionException e) {
-        e = null;
-      }
-        it.configure(new String [] {"-user", "adam@example.org", "-password", "bar", "-host", "cloud1.cggh.org", "-ticketGrantingServiceUrl", "http://cloud1.cggh.org/sso/v1/tickets"});
-
+    try {
+      AuthorisedCasProxy.configure(new String[] { "-us", "adam@example.org" });
+      fail("Should have bombed");
+    } catch (UnrecognizedOptionException e) {
+      e = null;
     }
+    AuthorisedCasProxy.configure(new String[] { "-user", "adam@example.org", "-password", "bar", "-host", "cloud1.cggh.org", "-ticketGrantingServiceUrl", "http://cloud1.cggh.org/sso/v1/tickets" });
 
+  }
 
 }
